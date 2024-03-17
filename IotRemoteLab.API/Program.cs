837fe85749d2cee6c353b2271d604ee6a743d885
Igor <igor.shaposhnikov@urfu.me>
2024-03-17 23:31:14 +0500

    Добавил SignalR hub.

--- IotRemoteLab.API/Program.cs
+++ IotRemoteLab.API/Program.cs
@@ -1,3 +1,5 @@
+using IotRemoteLab.API.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +23,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<StandHub>("/stand-hub");
 
 app.Run();
