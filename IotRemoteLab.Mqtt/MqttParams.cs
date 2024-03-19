3abb5e6950354db6d2d29938e6aa6cbbaa8d71c8
Igor <igor.shaposhnikov@urfu.me>
2024-03-19 15:54:31 +0500

    Добавил классы для работы с Mqtt

---
+++ IotRemoteLab.Mqtt/MqttParams.cs
@@ -0,0 +1,21 @@
+﻿namespace IotRemoteLaboratory.Mqtt.Core
+{
+    public sealed class MqttParams
+    {
+        public string Ip { get; }
+        public int Port { get; }
+        public IEnumerable<string> Topics { get; }
+
+        public MqttParams(string ip, int port, IEnumerable<string> topics)
+        {
+            Ip = ip;
+            Port = port;
+            Topics = topics;
+        }
+
+        public MqttParams(string ip, int port, params string[] topics) : this(ip, port, (IEnumerable<string>)topics)
+        {
+
+        }
+    }
+}
