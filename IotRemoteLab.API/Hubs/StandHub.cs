d49ee5a510c74b44995c73136a2274d8188f39b9
Igor <igor.shaposhnikov@urfu.me>
2024-03-19 16:34:02 +0500

    Добавил метод hub'а для работы с топиками

--- IotRemoteLab.API/Hubs/StandHub.cs
+++ IotRemoteLab.API/Hubs/StandHub.cs
@@ -4,5 +4,9 @@ namespace IotRemoteLab.API.Hubs
 {
     public class StandHub : Hub
     {
+        public async Task SendToTopic(string topic, string args) 
+        {
+            await Clients.All.SendAsync("StandStateChanged", topic, args);
+        }
     }
 }
