f022ed1c3922314acd805fbfbedd9423ac964f33
Igor <igor.shaposhnikov@urfu.me>
2024-03-31 23:20:37 +0500

    Добавил CheckBox

---
+++ IotRemoteLab.Blazor/Components/CheckBox.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace IotRemoteLab.Blazor.Components
+{
+    public partial class CheckBox : Component
+    {
+        #region Parameters
+
+
+        [Parameter]
+        public string? Name { get; set; }
+        /// <summary>
+        /// Состояние checkbox.
+        /// </summary>
+        [Parameter]
+        public bool IsChecked { get; set; }
+        /// <summary>
+        /// Текст.
+        /// </summary>
+        [Parameter]
+        public string Text { get; set; } = string.Empty;
+        /// <summary>
+        /// Callback об изменении состояния.
+        /// </summary>
+        [Parameter]
+        public EventCallback<Tuple<string, bool>> StateChanged { get; set; }
+
+
+        #endregion Parameters
+
+
+        #region Private Methods
+
+
+        void CheckboxChanged(MouseEventArgs e)
+        {
+            IsChecked = !IsChecked;
+            StateChanged.InvokeAsync(Tuple.Create<string, bool>(Text, IsChecked));
+            InvokeAsync(StateHasChanged);
+        }
+
+
+        #endregion Private Methods
+    }
+}
---
+++ IotRemoteLab.Blazor/Components/Component.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace IotRemoteLab.Blazor.Components
+{
+    public class Component : ComponentBase
+    {
+        #region Properties
+
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, object> Attributes { get; set; }
+
+        [Parameter]
+        public bool Visible { get; set; } = true;
+
+
+        public string UniqueId { get; set; }
+
+
+        #endregion Properties
+
+
+        #region Public & Protected Methods
+
+
+        protected override void OnInitialized()
+        {
+            UniqueId = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Replace('-', '-').Replace("+", "-").Substring(0, 10);
+        }
+
+
+        public string GetId() 
+        {
+            if (Attributes != null && Attributes.TryGetValue("id", out var id) && !string.IsNullOrEmpty(Convert.ToString(@id)))
+            {
+                return $"{@id}";
+            }
+
+            return UniqueId;
+        }
+
+        #endregion Public & Protected Methods
+    }
+}
