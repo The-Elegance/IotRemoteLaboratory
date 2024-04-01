d2a97489620699c9c45569ec8040b078429f4e74
Блаблаблаблааа <tokareva_todal@mail.ru>
2024-04-01 20:58:48 +0500

    Добавила формы для админки, начала настройку SignalR

--- IotRemoteLab.API/Hubs/StandHub.cs
+++ IotRemoteLab.API/Hubs/StandHub.cs
@@ -1,8 +1,35 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Numerics;
+using IotRemoteLab.Domain;
+using IotRemoteLab.Domain.Stand;
 
 namespace IotRemoteLab.API.Hubs
 {
     public class StandHub : Hub
     {
+        public async Task AddFramework(McuFramework framework)
+        {
+            await Clients.All.SendAsync("FrameworkAdded", framework);
+        }
+
+        public async Task AddMcu(Mcu mcu)
+        {
+            await Clients.All.SendAsync("McuAdded", mcu);
+        }
+
+        public async Task AddPort(McuFramework newFramework)
+        {
+            await Clients.All.SendAsync("FrameworkAdded", newFramework);
+        }
+
+        public async Task AddBenchTable(McuFramework newFramework)
+        {
+            await Clients.All.SendAsync("FrameworkAdded", newFramework);
+        }
+
+        public async Task AddStand(McuFramework newFramework)
+        {
+            await Clients.All.SendAsync("FrameworkAdded", newFramework);
+        }
     }
 }
