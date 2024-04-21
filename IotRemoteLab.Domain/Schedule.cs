75e80234105f6c592a3ed713e839809056a3884a
IuxMorti <zdracte21@bk.ru>
2024-04-21 18:01:51 +0500

    добавил модельку расписания

---
+++ IotRemoteLab.Domain/Schedule.cs
@@ -0,0 +1,10 @@
+﻿namespace IotRemoteLab.Domain;
+
+public sealed record Schedule
+{
+    public Guid Id { get; init; }
+    public Team.Team Team { get; init; }
+    public IReadOnlyList<Stand.Stand> Stands { get; init; }
+    public DateTime Start { get; init; }
+    public DateTime End { get; init; }
+}
