37622516f667fa912155ed2030469d2cc225a2ef
Блаблаблаблааа <tokareva_todal@mail.ru>
2024-04-28 18:45:26 +0500

    Админка

--- IotRemoteLab.Blazor/Program.cs
+++ IotRemoteLab.Blazor/Program.cs
@@ -1,6 +1,8 @@
 using IotRemoteLab.Blazor;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
 using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,5 +13,13 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<Radzen.DialogService>();
 builder.Services.AddRadzenComponents();
+
+builder.Services.AddSingleton(sp => {
+    var navigationManager = sp.GetRequiredService<NavigationManager>();
+    return new HubConnectionBuilder()
+      .WithUrl("https://localhost:7216/stand-hub")
+      .WithAutomaticReconnect()
+      .Build();
+});
 
 await builder.Build().RunAsync();
