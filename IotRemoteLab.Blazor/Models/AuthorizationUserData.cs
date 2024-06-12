66646e99d7541a65415ce16c783cc56b1cd31232
Igor <igor.shaposhnikov@urfu.me>
2024-06-12 19:48:31 +0500

    Поправил форму регистрации

---
+++ IotRemoteLab.Blazor/Models/AuthorizationUserData.cs
@@ -0,0 +1,8 @@
+﻿namespace IotRemoteLab.Blazor.Models
+{
+    public class AuthorizationUserData
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ IotRemoteLab.Blazor/Models/RegistrationUserData.cs
@@ -0,0 +1,11 @@
+﻿namespace IotRemoteLab.Blazor.Models
+{
+	public class RegistrationUserData
+	{
+		public string Login { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
