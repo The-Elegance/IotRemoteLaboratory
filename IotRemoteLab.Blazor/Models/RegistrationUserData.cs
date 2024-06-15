7d9fd51d7b6ca38da1b1a27368d8892b5cce9533
Igor <igor.shaposhnikov@urfu.me>
2024-06-16 00:45:52 +0500

    Допилил верстку для Auth/Register страниц

--- IotRemoteLab.Blazor/Models/RegistrationUserData.cs
+++ IotRemoteLab.Blazor/Models/RegistrationUserData.cs
@@ -6,12 +6,12 @@ namespace IotRemoteLab.Blazor.Models
 	{
 		[MinLength(4)]
 		public string Login { get; set; }
-		[MinLength(1)]
-		public string Name { get; set; }
-		[MinLength(1)]
-		public string Surname { get; set; }
-		[DataType(DataType.EmailAddress)]
-		[EmailAddress]
+		//[MinLength(1)]
+		//public string Name { get; set; }
+		//[MinLength(1)]
+		//public string Surname { get; set; }
+		//[DataType(DataType.EmailAddress)]
+		//[EmailAddress]
 		public string Email { get; set; }
 		[MinLength(1)]
 		public string Password { get; set; }
