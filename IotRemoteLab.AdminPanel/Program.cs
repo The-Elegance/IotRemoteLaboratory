7da2a1cc0542ae7d7c9f34019866c34e29f35a8c
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2024-11-13 14:41:58 +0500

    Полный переход на ant

--- IotRemoteLab.AdminPanel/Program.cs
+++ IotRemoteLab.AdminPanel/Program.cs
@@ -1,15 +1,16 @@
 using IotRemoteLab.AdminPanel;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7216")
+});
 
 builder.Services.AddAntDesign();
-builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
