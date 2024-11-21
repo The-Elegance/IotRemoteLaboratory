7cf709fd827e96e5e8109db2b50a632f9fac9a05
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2024-11-21 22:43:00 +0500

    Компоненты для работы с UTC

---
+++ IotRemoteLab.Common/Time/InitializeTimeZone.cs
@@ -0,0 +1,29 @@
+﻿using IotRemoteLab.Common.Provider;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace IotRemoteLab.Common.Time
+{
+    public sealed class InitializeTimeZone : ComponentBase
+    {
+        [Inject] public BrowserTimeProvider TimeProvider { get; set; } = default!;
+        [Inject] public IJSRuntime JSRuntime { get; set; } = default!;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender && TimeProvider is BrowserTimeProvider browserTimeProvider && !browserTimeProvider.IsLocalTimeZoneSet)
+            {
+                try
+                {
+                    await using var module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "/js/timezone.js");
+                    var timeZone = await module.InvokeAsync<string>("getBrowserTimeZone");
+                    browserTimeProvider.SetBrowserTimeZone(timeZone);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+        }
+    }
+}
