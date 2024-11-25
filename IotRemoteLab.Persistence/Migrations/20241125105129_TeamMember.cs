f0c4f897918add8bb6aea78b96b7c796bee644c5
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2024-11-25 15:55:08 +0500

    Таблица TeamUser

---
+++ IotRemoteLab.Persistence/Migrations/20241125105129_TeamMember.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IotRemoteLab.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TeamMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropForeignKey(
+            //    name: "FK_Users_Teams_TeamId",
+            //    table: "Users");
+
+            //migrationBuilder.DropIndex(
+            //    name: "IX_Users_TeamId",
+            //    table: "Users");
+
+            //migrationBuilder.DropColumn(
+            //    name: "TeamId",
+            //    table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "TeamUser",
+                columns: table => new
+                {
+                    TeamId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamUser", x => new { x.TeamId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_TeamUser_Team",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TeamUser_User",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TeamUser_UserId",
+                table: "TeamUser",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TeamUser");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TeamId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_TeamId",
+                table: "Users",
+                column: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Teams_TeamId",
+                table: "Users",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "Id");
+        }
+    }
+}
