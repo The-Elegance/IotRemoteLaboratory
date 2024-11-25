88d41e846d4109d3647b0deb23265a66260d9ef6
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2024-11-25 18:27:38 +0500

    TPH для Schedule

---
+++ IotRemoteLab.Domain/User.cs
@@ -0,0 +1,14 @@
+﻿using IotRemoteLab.Domain.Schedule;
+using System.ComponentModel.DataAnnotations;
+
+namespace IotRemoteLab.Domain;
+public class User : IScheduleHolder
+{
+    [Required] public Guid Id { get; set; }
+    [Required] public string Email { get; set; }
+    [Required] public string PasswordHash { get; set; }
+    [Required] public string Name { get; set; }
+    public string? Surname { get; set; }
+    [Required] public string? GroupNumber { get; set; }
+    [Required] public IReadOnlyList<Role.Role> Roles { get; set; }
+}
