7fa844841da445a2eea0e8f736acf8e10c2dfa99
Блаблаблаблааа <tokareva_todal@mail.ru>
2024-12-11 17:14:43 +0500

    fix2

---
+++ IotRemoteLab.API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Asp.Versioning;
+using IotRemoteLab.Domain.User;
+using IotRemoteLab.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IotRemoteLab.API.Controllers
+{
+    [ApiVersion(1.0)]
+    //[Authorize]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private ApplicationContext _context;
+
+
+        public UsersController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetUser()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
